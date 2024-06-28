37ec7028168a700b8b2306309ec701388b02160b
Nico <akebra77@gmail.com>
2024-06-28 15:23:28 +0200

    Added components

--- clonex/src/app/page.tsx
+++ clonex/src/app/page.tsx
@@ -1,17 +1,34 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { AuthButtonServer } from '@/app/components/auth-button-server'
+import { redirect } from 'next/navigation'
+import { PostLists } from './components/posts-list'
+import { type Database } from './types/database'
+import { ComposePost } from './components/compose-post'
+import { type Post } from './types/posts'
 
 
-export default  async function Home() {
-const supabase = createServerComponentClient({ cookies })
-const { data: posts } = await supabase.from('posts').select('*')
+export default async function Home () {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (session === null) {
+    redirect('/login')
+  }
+
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, user:users(name, avatar_url, username)')
+    .order('created_at', { ascending: false })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola!
+    <main className="flex min-h-screen flex-col items-center justify-between">
 
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
-          </main>
-  );
+      <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
+        <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
+        <PostLists posts={posts as Post[]} />
+      </section>
+      <AuthButtonServer />
+    </main>
+  )
 }
-
