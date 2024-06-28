279fd880f0b7122931ec146d038958496c582cae
Nico <akebra77@gmail.com>
2024-06-28 08:31:37 +0200

    Supabase configuration set up

--- clonex/src/app/page.tsx
+++ clonex/src/app/page.tsx
@@ -1,8 +1,16 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
 
-export default function Home() {
+
+export default  async function Home() {
+const supabase = createServerComponentClient({ cookies })
+const { data: posts } = await supabase.from('posts').select('*')
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hola!
+
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
           </main>
   );
 }
